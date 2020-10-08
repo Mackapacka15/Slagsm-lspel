ebc9a82676b9e1c4ca3431e1d2f1aba2df5dd388
Markus <Markuspiotrowski@live.com>
2020-10-08 14:23:14 +0200

    Första

---
+++ Slagsmål/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Slagsmål
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp1=100;
+            int hp2=100;
+            string Namn1="Spelare 1";
+            string Namn2="Spelare 2";
+            bool liv=true;
+            while (liv==true){
+                Random generator=new Random();
+                
+            }
+        }
+    }
+}
